386853f36d0598d75751b0734c0c46d5e283dea1
Matthew von Rocketstein <matt@telemachus.(none)>
2011-02-16 00:45:02 -0800

    moving functions to mixin

--- lib/kinbaku/codebase/codebase.py
+++ lib/kinbaku/codebase/codebase.py
@@ -11,9 +11,9 @@ from rope.base.project import Project
 from rope.refactor import restructure
 
 from kinbaku import analysis
-
+from kinbaku.plugin import KinbakuPlugin
 from kinbaku.util import report, is_python, groupby
-from kinbaku.codebase.bases import CBPlugin, Sandbox, CBContext
+from kinbaku.codebase.bases import Sandbox, CBContext
 from kinbaku._types import UnusableCodeError
 from kinbaku.plugin import publish_to_commandline
 
@@ -27,19 +27,7 @@ def map_over_files(func):
         return dict( [ [fpath, func(fpath)] for fpath in self.files(*args, **kargs) ] )
     return likefilelines
 
-
-class CodeBase(CBContext, Sandbox, CBPlugin):
-    """ a thin wrapper on rope.base.project for easily working with sandboxes """
-
-    debug          = False
-    count_files    = property(lambda self: len(self.files()))
-    count_py_files = property(lambda self: len(self.files(python=True)))
-    count_lines    = property(lambda self: sum(self.file_lines(python=True).values()))
-    file_lines     = map_over_files(analysis.count_lines)
-    word_summary   = map_over_files(analysis.word_summary)
-    file_comments  = map_over_files(analysis.get_comments)
-    file_flakes    = map_over_files(analysis.pyflakes)
-
+class CBPlugin(KinbakuPlugin):
     @classmethod
     def spawn(kls, path=None, **kargs):
         """ """
@@ -55,6 +43,32 @@ class CodeBase(CBContext, Sandbox, CBPlugin):
         obj = kls(path, gloves_off=True)
         #print 'returning obj',obj
         return obj
+    @publish_to_commandline
+    def search(self, name):
+        """ ex: kinbaku codebase search "zam" """
+        result = self._search(name)
+
+        #out=[]
+        out={}
+        for match1 in result.get('change_map',[]):
+            for match2 in match1['matches']:
+               #out.append(Match(search=name,**match2))
+               #print "{path}\n{lineno}:{line}".format(path=match2['real_path'],
+               #                                       lineno=match2['lineno'],
+               #                                       line=match2['line'])
+               entry = "{lineno}:{line}".format(\
+                   lineno=match2['lineno'],
+                   line=match2['line'])
+
+               if match2['real_path'] in out: out[match2['real_path']] += [entry]
+               else:                          out[match2['real_path']] = [entry]
+
+        for k in out:
+            print
+            print k
+            for x in out[k]: print x
+            print
+        return
 
     @publish_to_commandline
     def names(self):
@@ -87,42 +101,11 @@ class CodeBase(CBContext, Sandbox, CBPlugin):
                      valid=[pylint(fpath) for fpath in self.files(python=True)],
                     )
 
-    def __init__(self, root, workspace=None, gloves_off=False, **rope_project_options):
+    @publish_to_commandline
+    def shell(self):
         """ """
-        def create_shadow(self):
-            """ returns path to a shadow of root """
-            name         = workspace or DEFAULT_WORKSPACE_NAME
-            shade_holder = CodeBase.shadow_container()
-            path         = os.path.join(shade_holder, name)
-
-            if os.path.exists(path):
-                if gloves_off:
-                    if self.debug: report("Gloves are off, killing ",path)
-                    #remove_recursively(path)
-                else:
-                    err = "If the gloves aren't off, the shadow ({sh})should be uninhabited.."
-                    raise Exception, err.format(sh=path)
-            return path
-
-        self.pth_root   = root
-        self.pth_shadow = create_shadow(self)
-        self.project    = Project(self.pth_shadow, **rope_project_options)
-
-    def snapshot(self, static=True):
-        """ take snapshot of current codebase """
-        [ self>>fpath for fpath in self.files(python=True) ]
-
-    def has_changed(self, fpath):
-        """
-        """
-        before       = open(fpath,'r').read()
-        after        = open((self^fpath),'r').read()
-        file_changed = not( before == after )
-        ast_changed  = compiler.parse(before,'exec')==compiler.parse(after,'exec')
-
-        out = dict( file_changed=file_changed,
-                    ast_changed=ast_changed )
-        return out
+        #print self.get_changes('${A}=${B}','${A} = ${B}',{}).changes[1].new_contents
+        from IPython import Shell; Shell.IPShellEmbed(argv=['-noconfirm_exit'])()
 
     @publish_to_commandline
     def files(self, python=False):
@@ -141,47 +124,90 @@ class CodeBase(CBContext, Sandbox, CBPlugin):
             out = all_files
         return out
 
+
+class CodeBase(CBContext, Sandbox, CBPlugin):
+    """ a thin wrapper on rope.base.project for easily working with sandboxes """
+
+    debug          = False
+    count_files    = property(lambda self: len(self.files()))
+    count_py_files = property(lambda self: len(self.files(python=True)))
+    count_lines    = property(lambda self: sum(self.file_lines(python=True).values()))
+    file_lines     = map_over_files(analysis.count_lines)
+    word_summary   = map_over_files(analysis.word_summary)
+    file_comments  = map_over_files(analysis.get_comments)
+    file_flakes    = map_over_files(analysis.pyflakes)
+
+    def __init__(self, root, workspace=None, gloves_off=False, **rope_project_options):
+        """ """
+        def create_shadow(self):
+            """ returns path to a shadow of root """
+            name         = workspace or DEFAULT_WORKSPACE_NAME
+            shade_holder = CodeBase.shadow_container()
+            path         = os.path.join(shade_holder, name)
+
+            if os.path.exists(path):
+                if gloves_off:
+                    if self.debug:
+                        report("Gloves are off, killing ",path)
+                    #remove_recursively(path)
+                else:
+                    err = "If the gloves aren't off, the shadow ({sh})should be uninhabited.."
+                    raise Exception, err.format(sh=path)
+            return path
+
+        self.pth_root   = root
+        self.pth_shadow = create_shadow(self)
+        self.project    = Project(self.pth_shadow, **rope_project_options)
+
+    def snapshot(self, static=True):
+        """ take snapshot of current codebase """
+        target_python_files = self.files(python=True)
+        for fpath in target_python_files:
+            report("Copying fpath @ {fpath} into shadow".format(fpath=fpath))
+            self>>fpath
+        shadow_files  = path(self.pth_shadow).files()
+        shadow_python = filter(is_python, shadow_files)
+        for fpath in shadow_python:
+            if fpath.basename() not in [x.basename() for x in target_python_files]:
+                report("Removing stale file @ {fpath}".format(fpath=fpath))
+                os.remove(fpath)
+
+    def has_changed(self, fpath):
+        """ answer whether has_changed(fpath) since list mirror """
+        before       = open(fpath,'r').read()
+        after        = open((self^fpath),'r').read()
+        file_changed = not( before == after )
+        ast_changed  = compiler.parse(before,'exec')==compiler.parse(after,'exec')
+
+        out = dict( file_changed=file_changed,
+                    ast_changed=ast_changed )
+        return out
+
     @property
-    def python_files(self): return self.files(python=True)
+    def python_files(self):
+        """ only python files in target dir """
+        return self.files(python=True)
 
     def __iter__(self):
         """ azucar syntactico: iterator over all files """
         return iter(self.files())
 
-    @publish_to_commandline
-    def search(self, name):
-        """ ex: kinbaku codebase search "zam" """
-        result = self._search(name)
 
-        #out=[]
-        out={}
-        for match1 in result.get('change_map',[]):
-            for match2 in match1['matches']:
-               #out.append(Match(search=name,**match2))
-               #print "{path}\n{lineno}:{line}".format(path=match2['real_path'],
-               #                                       lineno=match2['lineno'],
-               #                                       line=match2['line'])
-               entry = "{lineno}:{line}".format(\
-                   lineno=match2['lineno'],
-                   line=match2['line'])
+    def restructure(self, pattern,goal, rules):
+        """ proxy to rope """
+        strukt  = restructure.Restructure(self.project, pattern, goal, rules)
+        return strukt
 
-               if match2['real_path'] in out: out[match2['real_path']] += [entry]
-               else:                          out[match2['real_path']] = [entry]
-
-        for k in out:
-            print
-            print k
-            for x in out[k]: print x
-            print
-        return
-
+    def get_changes(self,pattern, goal, rules):
+        """ proxy to rope """
+        return self.restructure(pattern, goal, rules).get_changes()
 
     def _search(self, name):
         """  internal search method
-             NOTE: example_rules = {'s': {'type': '__builtins__.str','unsure': True}})
+               NOTE: example_rules = {'s': {'type': '__builtins__.str','unsure': True}})
         """
         def get_matches(sections):
-            """ """
+            """ obtain match objections inside sections """
             matches  = []
             for diff_marker, line in sections:
                 junk1, data1,data2, junk2 = diff_marker.split()
@@ -200,8 +226,12 @@ class CodeBase(CBContext, Sandbox, CBPlugin):
         self.snapshot()
 
         ## Setup and grab data from rope-restructuring
-        strukt  = restructure.Restructure(self.project, pattern, goal, rules)
-        changes = strukt.get_changes()
+        #strukt  = restructure.Restructure(self.project, pattern, goal, rules)
+        #changes = strukt.get_changes()
+        #strukt  = self.restructure(pattern,goal,rules)
+        changes = self.get_changes(pattern,goal,rules)
+
+
         descr   = changes.description
         out     = dict( pattern=pattern, goal=goal, rules=rules,
                         descr=descr, changes=changes,)
