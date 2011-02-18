b106994ebc5d0886bb10da0f8b175a4bc1d1ec41
mvr <matthewvonrocketstein@gmail-dot-com>
2011-02-17 20:49:36 -0500

    dynamic optionparser creation from function signature

--- lib/kinbaku/plugin.py
+++ lib/kinbaku/plugin.py
@@ -3,11 +3,17 @@
 
 import copy
 import sys, os
-
+import inspect
 from path import path
 from pep362 import signature
 
 from kinbaku.report import report
+
+def options2dictionary(options):
+    """ conversion to dictionary  from an optparser.options instance """
+    options         = eval(str(options))
+    #options['path'] = path
+    return options
 
 def publish_to_commandline(func):
     """ decorator for plugin methods """
@@ -17,51 +23,66 @@ def is_published_to_commandline(func):
     """ detects functions that have been marked for publishcation """
     return hasattr(func,'is_published_to_commandline')
 
-class Plugin(object):
+
+def panic(kls):
+    print
+    kls().help()
+    sys.exit()
+
+def get_path_from_config():
+    # NOTE: do not move import..
+    from kinbaku.config import Config
+    # Get "path" by way of the Config-plugin.
+    path = Config().get('path', os.getcwd())
+    if not os.path.exists(path):
+        path = os.getcwd()
+    return path
+
+from types import BooleanType
+from optparse import OptionParser
+def oparser_from_sig(func_sig):
     """ """
+    class Foo(OptionParser):
+        pass
+    obj=Foo()
+    for name,val in func_sig.default_values.items():
+        kargs = dict(dest=name, metavar=name.upper())
+        if isinstance(val,BooleanType):
+            if val: kargs.update(dict(default=val, action="store_false"))
+            else:   kargs.update(dict(default=val, action="store_true"))
+        else:
+            raise Exception
+        obj.add_option("--"+name, **kargs)
+
+    return obj
+
+class Plugin(object):
+    """ Abstract Plugin """
     @classmethod
     def parse_args(kls, args, options):
         """ receives cascaded args/options """
 
-        def panic():
-            print
-            #print kls.__module__.split('.')[-1].upper()
-            kls().help()
-            sys.exit()
-
-        # NOTE: do not move import..
-        from kinbaku.config import Config
 
         try: interface_name = args[0]
         except IndexError: ## Not even a function name
-            panic()
+            panic(kls)
         else: ## Got plugin name..
             try: args = args[1:]
             except IndexError: ## .. But not command.
                 show_plugin_commands(interface_name)
                 sys.exit()
 
-
             else: ## Got name and command!
                 pass
 
-        # Get "path" either from command line or
-        #  by way of the Config-plugin.
-        path = options.path
-        path = path or Config().get('path', os.getcwd())
+        path = get_path_from_config()
 
         if not (interface_name and path):
-            print 'wonk'
             from kinbaku.bin.kbk import USAGE
             print USAGE
             sys.exit()
 
-        if not os.path.exists(path):
-            path = os.getcwd()
-
-        # conversion to dictionary  from an optparser.options instance
-        options = eval(str(options))
-        options['path']=path
+        options = options2dictionary(options)
         try:
             instance = kls.spawn(**options)
         except TypeError:
@@ -70,27 +91,43 @@ class Plugin(object):
             sys.exit()
 
         func   = getattr(instance, interface_name,None)
-        if func is None:
-            panic()
+        if func is None: panic(kls)
+        func_sig = signature(func)
+        if func_sig.has_default_values:
+            # discards the arguments, keeping original
+            parser     = oparser_from_sig(func_sig)
+            options, _ = parser.parse_args()
+            options    = options2dictionary(options)
+            kargs      = options
+        else:
+            kargs = {}
+
+
         parent = kls.__name__.lower()
         if not is_published_to_commandline(func):
             err  = '\nERROR:  Interface "{I}" is not published on plugin "{P}".'
             err += '\n        For help using this plugin, try: "{progname} {P} help"'
-            err  = err.format(I=interface_name, P=parent,progname=sys.argv[0],
-                              )
+            fmt  = dict(I=interface_name, P=parent,progname=sys.argv[0],)
+            err  = err.format(**fmt)
             print err
             sys.exit()
 
         #report("running {f} with {a}, {k}", f=func.__name__, a=args, k=options)
-        kls.display_results(func(*args))
+        #print '*'*80,args, kargs
+        result = func(*args, **kargs)
+        kls.display_results(result)
+
+    def get_subcommands(self):
+        """ """
+        return [ x for x in dir(self.__class__) if \
+                 is_published_to_commandline(getattr(self, x)) and\
+                 x!='help' ]
 
     @publish_to_commandline
     def help(self, indent=0):
         """ shows help for this plugin """
-        cls_names = [ x for x in dir(self.__class__) if \
-                      is_published_to_commandline(getattr(self, x)) and\
-                      x!='help' ]
-        modname    = self.__class__.__module__.lower().split('.')[-1]
+        cls_names = self.get_subcommands()
+        modname   = self.__class__.__module__.lower().split('.')[-1]
 
         if not indent:
             print "->",modname.upper()
@@ -107,7 +144,7 @@ class Plugin(object):
 
             display1     = lambda k:   '<{k}>'.format(k=k)
             dvdisplay    = lambda p:   (p.default_value is not None and \
-                                        '[<{k}={deflt}>'.format(**{'k':p.name,
+                                        '[<{k}={deflt}>]'.format(**{'k':p.name,
                                                                   'deflt':p.default_value})) or \
                                        ('[<{k}>]'.format(**{'k':p.name}))
             position     = lambda k:   parameters[k].position
@@ -116,10 +153,11 @@ class Plugin(object):
             func_sig   = [ [position(k), k, parameters[k] ] for k in parameters.keys() ]
             func_sig.sort(sort_machine) # sort args by their position in signature
 
-            display    = lambda t: (hasattr(t[2],'default_value') and \
-                                    dvdisplay(t[2])) or \
-                                   (True and \
-                                    display1(t[1]))
+            def display(t):
+                if hasattr(t[2],'default_value'):
+                    return dvdisplay(t[2])
+                else:
+                    return display1(t[1])
 
             func_sig   = [ display(x) for x in func_sig ]
             func_sig   = func_sig[1:] # probably an instancemethod.. chop off "self"
@@ -147,7 +185,7 @@ class Plugin(object):
                 if first_loop:
                     print '\n\t'+_ex
                 print '\t'+out + line or ""
-
+        return cls_names
 
     @classmethod
     def spawn(kls, **kargs): return kls(**kargs)
