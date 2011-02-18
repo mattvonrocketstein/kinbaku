f5b3de0596b7e0edd0d027dbf06f37cc34e06178
Matt <matt.anderson@cmgdigital.com>
2011-02-17 20:50:39 -0500

    subclassing pep362 function-signature obj to make it better

--- lib/kinbaku/_types.py
+++ lib/kinbaku/_types.py
@@ -2,6 +2,23 @@
 """
 from kinbaku.report import console, report
 
+# TODO: find a way around this monkey patch
+import pep362
+from pep362 import Signature as JohnHancock
+class Signature(JohnHancock):
+    def __len__(self):
+        return len(self._parameters)
+
+    @property
+    def has_default_values(self):
+        return any([hasattr(p,'default_value') for p in self._parameters.values()])
+
+    @property
+    def default_values(self):
+        items = [[k,v] for k,v in self._parameters.items() if hasattr(v,'default_value')]
+        return dict([[k, v.default_value] for k,v in items])
+pep362.Signature=Signature
+
 class UnusableCodeError(ValueError):
     pass
 
