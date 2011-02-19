f32e4febd5d6c9a47b09081c1063b8b00cc9d119
Matthew von Rocketstein <matt@telemachus.(none)>
2011-02-19 14:51:23 -0800

    correct comment/code comment-groups count

--- lib/kinbaku/comments.py
+++ lib/kinbaku/comments.py
@@ -73,7 +73,7 @@ class CommentsExtractor(KinbakuPlugin):
 
     @publish_to_commandline
     def ratio(self, input_file_or_dir):
-        """ """
+        """ extract comment:code ratios """
         if path(input_file_or_dir).isdir():
             for fpath in [x for x in path(input_file_or_dir).files() if x.endswith('.py')]:
                 print fpath
@@ -82,7 +82,7 @@ class CommentsExtractor(KinbakuPlugin):
                 print
             return
 
-        groups = self._extract(input_file_or_dir)
+        fpath,groups = self._extract(input_file_or_dir)
         if groups:
             count        = [ [1 for comment in group] for group in groups]
             num_comments = sum([sum(subcount) for subcount in count ])
@@ -94,7 +94,7 @@ class CommentsExtractor(KinbakuPlugin):
             msg2 = "  total_comment_lines {N}".format(N=num_comments)
             msg3 = "  total_comments {K}".format(K=len(groups))
             msg4 = "  avg_lines_group {M}".format(M=num_comments*1.0/len(groups))
-
+            #from IPython import Shell; Shell.IPShellEmbed(argv=['-noconfirm_exit'])()
             print msg0
             print msg3
             print msg1
@@ -102,43 +102,43 @@ class CommentsExtractor(KinbakuPlugin):
             print msg4
 
     @publish_to_commandline
-    def extract(self, input_file_or_dir):
-        """ extracts both module/class/function documentation,
-            as well as comments and displays them in order """
-        def display_file(fpath):
-            """ """
-            print console.blue('{fpath}'.format(fpath=fpath))
-        print input_file_or_dir
-        fpath, lst_of_comment_lsts = self._extract(input_file_or_dir)
-        ## display results
-        if True:
-            display_file(fpath)
-            for comment_lst in lst_of_comment_lsts:
-                #from IPython import Shell; Shell.IPShellEmbed(argv=['-noconfirm_exit'])()
-                owner_display = console.blue(comment_lst[0].rowner())
-                print '\nin "{owner}":'.format(owner=owner_display)
-                print '\n'.join([str(comment) for comment in comment_lst])
+    def extract(self, input_file_or_dir, comments=False,docstrings=False):
+        """ extract comments, organized by container.
+            if --docstrings is True, only docstring-style comments will be displayed.
+            if --comments is True, only hash-mark style comments will be displayed.
+            default is to show both.
+        """
+        fpath, lst_of_comment_lsts = self._extract(input_file_or_dir,comments=comments,docstrings=docstrings)
 
-    def _extract(self, input_file_or_dir):
+        ## display results
+        print console.blue('{fpath}'.format(fpath=fpath))
+        for comment_lst in lst_of_comment_lsts:
+            owner_display = console.blue(comment_lst[0].rowner())
+            print '\nin "{owner}":'.format(owner=owner_display)
+            print '\n'.join([str(comment) for comment in comment_lst])
+
+    def _extract(self, input_file_or_dir, comments=True, docstrings=True):
         """ """
+        if not comments and not docstrings:
+            comments = docstrings = True
         with CodeBase(input_file_or_dir, gloves_off=True, workspace=None) as codebase:
             self.codebase = codebase
-            #print codebase.files()
             for fpath in codebase.python_files:
                 # TODO: validate file
-
                 fhandle       = lambda: open(fpath)
                 content_raw   = fhandle().read()
                 content_lines = fhandle().readlines()
 
-                docstrings    = extract_docstrings(content_raw, fpath.name)
-                comments    = extract_comments(content_lines)
+                doc_strings = extract_docstrings(content_raw, fpath.name)
+                komments    = extract_comments(content_lines)
 
                 ## merge lists by line number
-                out = docstrings + comments
-                if docstrings and comments:
-                    out.sort(lambda x,y: cmp(x[0].lineno, y[0].lineno))
-
-        return fpath, out
-
+                out = []
+                if docstrings: out+=doc_strings
+                if comments: out+=komments
+                out.sort(lambda x,y: cmp(x[0].lineno, y[0].lineno))
+                return fpath, out
 plugin = CommentsExtractor
+
+if __name__=='__main__':
+    from IPython import Shell; Shell.IPShellEmbed(argv=['-noconfirm_exit'])()
