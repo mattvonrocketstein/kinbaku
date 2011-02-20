5521bd123cd06f01af93dd70d45e4dcc4035380d
mvr <matthewvonrocketstein@gmail-dot-com>
2011-02-20 00:00:01 -0800

    adding another test-file to codebox

--- codebox/simple_python_file.py
+++ codebox/simple_python_file.py
@@ -1,5 +1,10 @@
 """ simple_python_file
+
+    what people are saying about this file:
+      pep8:
+        codebox/simple_python_file.py:5:1: E302 expected 2 blank lines, found 1
 """
+import os
 
 def f():
     return 3
@@ -11,5 +16,6 @@ class Klass(object):
     """ docstring """
     def __init__(self):
         return
+
     def method(self):
         return os.path.exists
