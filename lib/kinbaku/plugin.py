677c0ff67018f4844c67b599e8a98e39a0579fa9
Matthew von Rocketstein <matt@telemachus.(none)>
2011-02-25 00:13:32 -0800

    better context help for commandline errors

--- lib/kinbaku/plugin.py
+++ lib/kinbaku/plugin.py
@@ -9,58 +9,26 @@ from types import BooleanType,StringType
 from optparse import OptionParser
 
 from path import path
-from kinbaku._types import Signature
 from pep362 import signature
 
-from kinbaku.report import report,console
+from kinbaku._types import Signature
+from kinbaku.plugin_helpers import panic
+from kinbaku.plugin_helpers import display
+from kinbaku.plugin_helpers import str2list
+from kinbaku.plugin_helpers import position
+from kinbaku.plugin_helpers import dvdisplay,func2sig
+from kinbaku.plugin_helpers import oparser_from_sig
+from kinbaku.plugin_helpers import options2dictionary
 
-def str2list(x,split=' '):
-    """ """
-    if isinstance(x,list):
-        return x
-    if isinstance(x, str):#StringType
-        return filter(None,x.split(split))
+from kinbaku.report import report, console
 
-position     = lambda parameters, k:   parameters[k].position
-display1     = lambda k:   '<{k}>'.format(k=k)
-def dvdisplay(p):
-    """ default-value display for parameter p """
-    fmt = {'k':p.name,
-           'deflt':p.default_value}
-    if p.default_value is not None:
-        return '[<{k}={deflt}>]'.format(**fmt)
-    else:
-        return ('[<{k}>]'.format(**{'k':p.name}))
-
-def prepare_sig(func,modname):
+def prepare_sig(func, modname):
     """ prepare a legible, context-senstive signature from func """
     progname   = os.path.split(sys.argv[0])[1]
-    sig        = signature(func)
-    parameters = sig._parameters
-
-    sort_machine = lambda x, y: cmp(x[0],y[0])
-    func_sig   = [ [position(parameters, k), k, parameters[k] ] for k in parameters.keys() ]
-    func_sig.sort(sort_machine) # sort args by their position in signature
-
-    func_sig   = [ display(x) for x in func_sig ]
-    func_sig   = func_sig[1:] # probably an instancemethod.. chop off "self"
-    func_sig   = ' '.join(func_sig)
-
-    _ex = "{kinbaku} {plugin} {name} {sig}"
-    _ex = _ex.format(kinbaku = progname, name = sig.name,
-                             plugin = modname, sig = func_sig)
-    return _ex
-
-def display(t):
-    if hasattr(t[2],'default_value'):
-        return dvdisplay(t[2])
-    else:
-        return display1(t[1])
-
-def options2dictionary(options):
-    """ conversion to dictionary  from an optparser.options instance """
-    options         = eval(str(options))
-    return options
+    sig_obj, sig_str = func2sig(func)
+    car =  "{kinbaku} ".format(kinbaku = progname)
+    cdr = '{plugin} {S}'.format(S=sig_str,plugin = modname, )
+    return car+cdr
 
 def publish_to_commandline(func):
     """ decorator for plugin methods """
@@ -71,10 +39,6 @@ def is_published_to_commandline(func):
     """ detects functions that have been marked for publishcation """
     return hasattr(func,'is_published_to_commandline')
 
-def panic(kls):
-    print; kls().help();
-    sys.exit()
-
 def get_path_from_config():
     """ Gets "path" by way of the Config-plugin.
         NOTE: do not move import..
@@ -84,25 +48,6 @@ def get_path_from_config():
     if not os.path.exists(path):
         path = os.getcwd()
     return path
-
-def oparser_from_sig(func_sig):
-    """ """
-    class Foo(OptionParser):
-        pass
-    obj=Foo()
-    for name,val in func_sig.default_values.items():
-        kargs = dict(dest=name, metavar=name.upper())
-        if isinstance(val,BooleanType):
-            if val: kargs.update(dict(default=val, action="store_false"))
-            else:   kargs.update(dict(default=val, action="store_true"))
-        elif isinstance(val, StringType):
-            kargs.update(default=val)
-        else:
-            kargs={} # abort.. not sure what to do yet
-            #raise Exception,[name,val]
-        if kargs:
-            obj.add_option("--"+name, **kargs)
-    return obj
 
 class Plugin(object):
     """ Abstract Plugin """
@@ -133,7 +78,7 @@ class Plugin(object):
             instance = kls.spawn(**options)
         except TypeError:
             print('\nERROR: spawning {kls} with {options}'.format(
-                kls=kls.__name__, options=options))
+                kls = kls.__name__, options=options))
             sys.exit()
 
         func   = getattr(instance, interface_name, None)
@@ -176,7 +121,12 @@ class Plugin(object):
             sys.exit()
 
         #print args,kargs
-        result = func(*args, **kargs) #kls.display_results(result)
+        try:
+            result = func(*args, **kargs) #kls.display_results(result)
+        except TypeError,t:
+            if func.func_name+'()' in str(t):
+                print " Usage: ",func2sig(func)[1]
+                sys.exit(1)
 
     def get_subcommands(self):
         """ """
