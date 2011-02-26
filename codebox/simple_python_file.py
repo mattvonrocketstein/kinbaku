76bbcd2ddcdae6931b26644ef96ca7c01cdb0503
mvr <matthewvonrocketstein@gmail-dot-com>
2011-02-25 22:49:32 -0800

    [scope] adding pylint output

--- codebox/simple_python_file.py
+++ codebox/simple_python_file.py
@@ -8,6 +8,33 @@
         codebox/simple_python_file.py:19: local variable 'a' is assigned to but never used
         codebox/simple_python_file.py:26: local variable 'x' is assigned to but never used
         codebox/simple_python_file.py:27: local variable 'x' is assigned to but never used
+      pylint:
+        C:  8: Line too long (90/80)
+        C:  9: Line too long (90/80)
+        C: 10: Line too long (90/80)
+        C: 20:Klass.method: Missing docstring
+        R: 20:Klass.method: Method could be a function
+        R: 15:Klass: Too few public methods (1/2)
+        W: 24:function_f: Redefining name 'a' from outer scope (line 31)
+        C: 23:function_f: Missing docstring
+        C: 24:function_f: Invalid name "a" (should match [a-z_][a-z0-9_]{2,30}$)
+        W: 24:function_f: Unused variable 'a'
+        C: 27:function_g: Missing docstring
+        C: 31:a: Invalid name "a" (should match [a-z_][a-z0-9_]{2,30}$)
+        C: 31:a: Missing docstring
+        C: 32:a.b: Invalid name "b" (should match [a-z_][a-z0-9_]{2,30}$)
+        C: 32:a.b: Missing docstring
+        C: 32:a.b: More than one statement on a single line
+        C: 32:a.b: Invalid name "x" (should match [a-z_][a-z0-9_]{2,30}$)
+        W: 32:a.b: Unused variable 'x'
+        C: 33:a: Operator not preceded by a space
+            x='in a()'; b()
+             ^
+        C: 33:a: Invalid name "x" (should match [a-z_][a-z0-9_]{2,30}$)
+        C: 33:a: More than one statement on a single line
+        W: 33:a: Unused variable 'x'
+        C: 34: More than one statement on a single line
+        C: 37: Invalid name "function_f" (should match (([A-Z_][A-Z0-9_]*)|(__.*__))$)
 
 """
 import os
