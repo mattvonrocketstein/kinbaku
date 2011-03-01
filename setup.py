d56c80aa992014438df764d7820df246dea444cc
mvr <matthewvonrocketstein@gmail-dot-com>
2011-02-28 21:58:16 -0800

    copied example from pygments.  works much better now

--- setup.py
+++ setup.py
@@ -1,26 +1,61 @@
 #!/usr/bin/env python
 """ setup.py for kinbaku
+
+      this pattern stolen from pygments setup.py
 """
 
 import os
 from setuptools import setup, find_packages
 
-BASEDIR = os.path.dirname(os.path.realpath(__file__))
+try:
+    from setuptools import setup, find_packages
+    have_setuptools = True
+except ImportError:
+    from distutils.core import setup
+    def find_packages():
+        return [
+            'kinbaku',
+            'kinbaku.core',
+            'kinbaku.report',
+            'kinbaku.plugin',
+            'kinbaku.comments',
+        ]
+    have_setuptools = False
+try:
+    from distutils.command.build_py import build_py_2to3 as build_py
+except ImportError:
+    from distutils.command.build_py import build_py
 
+if have_setuptools:
+    add_keywords = dict( entry_points = \
+                         { 'console_scripts': \
+                           ['kinbaku = kinbaku.bin.kbk:entry', \
+                            'kbk-comments = kinbaku.bin.kbk:comments'], \
+                         }, )
+else:
+    add_keywords = dict( scripts = ['kinbaku'], )
 
 setup(
     name         ='kinbaku',
     version      = '.1',
     description  = 'use cases for rope',
-
     author       = 'mattvonrocketstein, in the gmails',
     url          = 'one of these days',
-
+    license = 'BSD License',
     package_dir  = {'': 'lib'},
     packages     = find_packages('lib'),
-    entry_points = {
-        'console_scripts': [
-            'kinbaku = kinbaku.bin.kbk:entry',
-         ],
-    },
+    long_description = __doc__,
+    keywords = 'tests tracing profiling ast',
+    platforms = 'any',
+    zip_safe = False,
+    include_package_data = True,
+    classifiers = [
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Developers',
+        'Development Status :: 000 - Experimental',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Operating System :: OS Independent', ],
+    cmdclass = {'build_py': build_py},
+    **add_keywords
 )
